a8c9feb877b4275db987e08b1dd7b6079aaa1523
Leonardo Querci <leonardoquerciprado@gmail.com>
2026-06-13 14:38:55 -0300

    Alterações finais

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Clube-Social</title>
+    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
     <h1>Seja bem-vindo ao clube do IBTP</h1>
